994eceb6c66c070b3ebf685a25effb699b4a3bd6
ness25 <108854656+ness25@users.noreply.github.com>
2022-07-07 10:46:08 +0530

    Added Multiplication feature

--- simple_calculator.py
+++ simple_calculator.py
@@ -3,7 +3,10 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 add = num1+num2
+mul = num1*num2
 
 print(f"The total is {add}")
+
+print(f"The total is {mul}")
 
 print("Edited the file")
